8e9554904daf6f4f1de390cd31559e702f2ea429
Abishak Sharma <abishaksharma70@gmail.com>
2018-01-23 16:30:09 +1030

    Fixed

--- index.php
+++ index.php
@@ -47,7 +47,7 @@
       <head>  
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-           <title>Webslesson Tutorial | Append Data to JSON File using PHP</title>  
+           <title>Abishak Sharma - Append Data to JSON File using PHP</title>  
            <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
